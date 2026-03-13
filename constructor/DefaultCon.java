b43bbcf2f8903306c4e03ad73adba356475037d8
Varanasi Bhavitha <bhavithavaranasi630@gmail.com>
2026-03-13 18:15:05 +0530

    Add files via upload

---
+++ constructor/DefaultCon.java
@@ -0,0 +1,15 @@
+class DefaultCon
+{
+  int age;
+  String name;
+  DefaultCon()
+  {
+    age=19;
+    name="bhavitha";
+  }
+  public static void main(String args[])
+  {
+    DefaultCon obj=new DefaultCon();
+    System.out.println("age: "+obj.age+"name:"+obj.name);
+  }
+}
