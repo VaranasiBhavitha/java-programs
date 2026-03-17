c305a684e6bf87ba897e4865f36fc4201bdcd026
Varanasi Bhavitha <bhavithavaranasi630@gmail.com>
2026-03-17 19:53:39 +0530

    Add files via upload

---
+++ methods/withParameter.java
@@ -0,0 +1,12 @@
+class withParameter
+{
+  void display(String name)
+  {
+    System.out.println("" +name);
+  }
+  public static void main(String args[])
+  {
+     withParameter obj= new withParameter();
+     obj.display("Bhavi");
+  }
+}
