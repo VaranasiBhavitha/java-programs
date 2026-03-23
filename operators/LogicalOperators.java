9498abe220218d1f7cf24e86465187f4f9814f55
Varanasi Bhavitha <bhavithavaranasi630@gmail.com>
2026-03-23 19:12:46 +0530

    Add files via upload

---
+++ operators/LogicalOperators.java
@@ -0,0 +1,9 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        boolean x = true, y = false;
+
+        System.out.println(x && y); // false
+        System.out.println(x || y); // true
+        System.out.println(!x);     // false
+    }
+}
