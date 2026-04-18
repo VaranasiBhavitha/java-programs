fdcff2239e41711a6fd9db79a58f19bc87e6248b
Varanasi Bhavitha <bhavithavaranasi630@gmail.com>
2026-04-18 16:56:07 +0530

    Add user prompt for array size in MaxElement
    Added prompt for user to enter the array size before inputting elements.

--- Arrays/MaxElement.java
+++ Arrays/MaxElement.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class MaxElement {
     public static void main(String[] args) {
+        System.out.println("Enter the array size:");
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
