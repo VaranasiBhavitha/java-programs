4d8c87feb2ed41890f3ed150306d353acc30c685
Varanasi Bhavitha <bhavithavaranasi630@gmail.com>
2026-06-03 16:56:51 +0530

    Add files via upload

---
+++ Arrays/Array1.java
@@ -0,0 +1,10 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Array Elements:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
