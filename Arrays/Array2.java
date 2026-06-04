1bf2a5efc53c056b98f8b481e0c47bee4a10711f
Varanasi Bhavitha <bhavithavaranasi630@gmail.com>
2026-06-04 20:02:16 +0530

    Add files via upload

---
+++ Arrays/Array2.java
@@ -0,0 +1,10 @@
+public class Array2 {
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 15, 20, 25};
+
+        System.out.println("Traversing Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Element at index " + i + " = " + arr[i]);
+        }
+    }
+}
